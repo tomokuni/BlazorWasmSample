0d97a6f1ac7b2f943fd8cf0b281880cf54a352e3
tomokuni <tomokuni@users.noreply.github.com>
2023-07-07 20:18:50 +0900

    BlazorWasmSample

---
+++ BlazorApp/Client/Program.cs
@@ -0,0 +1,22 @@
+using BlazorApp;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using MudBlazor.Services;
+
+namespace BlazorApp;
+public class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    builder.Services.AddMudServices();
+
+    await builder.Build().RunAsync();
+  }
+}
